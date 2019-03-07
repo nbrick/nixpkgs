ac0050561c6e991de1b60cbd6c2f055a51cf5e5a
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-07 14:29:45 +0100

    osinfo-db-tools: 1.2.0 -> 1.4.0

--- pkgs/tools/misc/osinfo-db-tools/default.nix
+++ pkgs/tools/misc/osinfo-db-tools/default.nix
@@ -1,17 +1,22 @@
-{ stdenv, fetchurl, pkgconfig, intltool, glib, libxml2
-, libxslt, libarchive, bzip2, lzma
+{ stdenv, fetchurl, pkgconfig, gettext, glib, libxml2
+, libxslt, libarchive, bzip2, lzma, json-glib, perl
 }:
 
 stdenv.mkDerivation rec {
-  name = "osinfo-db-tools-1.2.0";
+  name = "osinfo-db-tools-1.4.0";
 
   src = fetchurl {
     url = "https://releases.pagure.org/libosinfo/${name}.tar.gz";
-    sha256 = "07zqbwsmdgnzqah2smm4zri04c0qm82z1jn8kzz1bnsqbfg84l1v";
+    sha256 = "08zpjq1g27n6wmmqwxws95w045warhg9bxcv1phvkdcrddf15q3y";
   };
 
-  nativeBuildInputs = [ pkgconfig intltool ];
-  buildInputs = [ glib libxml2 libxslt libarchive bzip2 lzma ];
+  nativeBuildInputs = [
+    pkgconfig gettext
+    perl # for pod2man command
+  ];
+  buildInputs = [
+    glib libxml2 libxslt libarchive bzip2 lzma json-glib
+  ];
 
   meta = with stdenv.lib; {
     description = "Tools for managing the osinfo database";
